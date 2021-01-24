d7fbb4ded546f095e16a7cf00ef1919e3e292cac
Robert Hardy <helloarchy@gmail.com>
2021-01-24 20:18:51 +0000

    Apply prettier to all files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,8 @@
-import '../styles/global.css'
-import {AppProps} from 'next/app'
+import "../styles/global.css";
+import { AppProps } from "next/app";
 
-function App({
-                 Component,
-                 pageProps
-             }: AppProps) {
-    return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
 
-export default App
+export default App;
